e0ee1bab490ba263ac917d33b9199659ac99db34
Smarth Sharma <131465300+smarthsharma07@users.noreply.github.com>
2026-07-06 16:32:44 +0530

    30 ms (66.67%) 72.5 MB (34.65%) 07-06-2026 Medium 0215-kth-largest-element-in-an-array .cpp

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int>q;
-        for (int x :nums){
-            q.push(x);
-        }
-        while(--k){
-            q.pop();
+        priority_queue<int,vector<int>,greater<int>> q;
+        for (int num :nums){
+            q.push(num);
+            if (q.size()>k){
+                q.pop();
+            }
         }
         return q.top();
     }
