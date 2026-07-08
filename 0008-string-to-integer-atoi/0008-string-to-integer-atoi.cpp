6e8e0c0450e0298cff48a403daabccb6c77029b9
Smarth Sharma <131465300+smarthsharma07@users.noreply.github.com>
2026-07-08 17:37:37 +0530

    0 ms (100%) 9.2 MB (55.88%) 07-08-2026 Medium 0008-string-to-integer-atoi .cpp

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        int n = s.length();
+        while(i<n && s[i]==' '){
+            i++;
+        }
+        int sign =1;
+        if (i<n && (s[i]=='+'||s[i]=='-')){
+            if (s[i]=='-'){
+                sign = -1;
+            }
+            i++;
+        }
+        long long num = 0;
+        while (i<n && isdigit(s[i])){
+            num=num*10+(s[i]-'0');
+            if(sign==1 && num>INT_MAX){
+                return INT_MAX;
+            }
+            if (sign==-1 && -num<INT_MIN){
+                return INT_MIN;
+            }
+            i++;
+        }
+        return (int)(sign*num);
+    }
+};
