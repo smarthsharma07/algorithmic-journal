f9bc0f1a9e6039160d31c4ca3ee8823ce48a571d
Smarth Sharma <131465300+smarthsharma07@users.noreply.github.com>
2026-07-10 20:48:05 +0530

    0 ms (100%) 21.9 MB (74.15%) 07-10-2026 Medium 0098-validate-binary-search-tree .cpp

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool dfs(TreeNode* root, long long low, long long high) {
+        if (root == nullptr)
+            return true;
+        if (root->val <= low || root->val >= high)
+            return false;
+        return dfs(root->left, low, root->val) &&
+               dfs(root->right, root->val, high);
+    }
+    bool isValidBST(TreeNode* root) {
+        return dfs(root, LLONG_MIN, LLONG_MAX);
+    }
+};
