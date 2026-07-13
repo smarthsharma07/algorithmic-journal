5ca7be2e71171e508ab8a2d509632b24d642e5be
Smarth Sharma <131465300+smarthsharma07@users.noreply.github.com>
2026-07-13 16:12:19 +0530

    0 ms (100%) 8.5 MB (18.24%) 07-13-2026 Medium 1291-sequential-digits .cpp

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        string s = "123456789";
+        vector<int>ans;
+        for (int i = 2 ; i<=9;i++){
+            for(int j = 0;j+i<=9;j++){
+                int num = stoi(s.substr(j, i));
+                if (num >= low && num <= high)
+                    ans.push_back(num);
+            }
+        }
+        return ans;
+    }
+};
