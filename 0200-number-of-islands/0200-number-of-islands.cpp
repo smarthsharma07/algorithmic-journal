db4d69d46fe35207a22ae041029b6d5cbb4c8b5b
Smarth Sharma <131465300+smarthsharma07@users.noreply.github.com>
2026-07-14 14:05:55 +0530

    34 ms (17.57%) 23.3 MB (27.71%) 07-14-2026 Medium 0200-number-of-islands .cpp

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void bfs(vector<vector<char>>& grid,int row, int col){
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        grid[row][col] = '0';
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+        while(!q.empty()){
+            auto [r,c] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if (nr >= 0 && nr < m &&
+                    nc >= 0 && nc < n &&
+                    grid[nr][nc] == '1') {
+
+                    grid[nr][nc] = '0';
+                    q.push({nr, nc});
+                }
+            }    
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int islands = 0; 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    bfs(grid, i, j);
+                }
+            }
+        }
+        return islands;
+    }
+};
