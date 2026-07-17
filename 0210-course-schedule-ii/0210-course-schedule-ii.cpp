c2cdfa02b24466f39d37e40723dcffea020e9033
Smarth Sharma <131465300+smarthsharma07@users.noreply.github.com>
2026-07-17 20:44:53 +0530

    5 ms (43.19%) 20.3 MB (5.25%) 07-17-2026 Medium 0210-course-schedule-ii .cpp

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> adj;
+    unordered_map<int, bool> visited;
+    unordered_map<int, bool> pathVisited;
+    vector<int> order;
+    bool dfs(int node) {
+        visited[node] = true;
+        pathVisited[node] = true;
+        for (int nei : adj[node]) {
+            if (!visited[nei]) {
+                if (dfs(nei))
+                    return true;
+            }
+            else if (pathVisited[nei]) {
+                return true;    
+            }
+        }
+        pathVisited[node] = false;
+        order.push_back(node);  
+        return false;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        for (auto &edge : prerequisites) {
+            adj[edge[1]].push_back(edge[0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i]) {
+                if (dfs(i))
+                    return {};   
+            }
+        }
+        reverse(order.begin(), order.end());
+        return order;
+    }
+};
